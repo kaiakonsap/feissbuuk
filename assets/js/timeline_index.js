2928ab726feaa5bf85a85782f988f24e06054ab0
kaia <kaia.konsap@khk.ee>
2013-05-28 16:37:53 +0300

    Old version

--- assets/js/timeline_index.js
+++ assets/js/timeline_index.js
@@ -1,33 +1,13 @@
 function publish() {
 
-	//retrieve comments to display on page
-
-		//add click handler for button
-		$("#add").click(function() {
-			alert("OOOO");
-			//define ajax config object
-			var ajaxOpts = {
-				type: "post",
-				url: "timeline.php",
-				data: "&comment=" + document.getElementById('post').value,
-				success: function(data) {
-
-					//create a container for the new comment
-					var div = $("<div>").addClass("row").appendTo("#comments");
-
-					// add author name and comment to container
-
-					//empty inputs
-					$("#post").find("textarea").val("");
-
-				}
-			};
-
-			$.ajax(ajaxOpts);
-
-		});
-
-
+	var text = document.getElementById('post').value;
+	var comment = [
+		'<div id="post2">',
+		'<p>',text, '</p>',
+		'</div>'
+	].join('');
+	$(comment).hide().appendTo('body').fadeIn();
+	alert("jehhei");
 
 
 }
--- views/timeline_index_view.php
+++ views/timeline_index_view.php
@@ -1,6 +1,6 @@
 <h1>MY POSTS ONLY</h1>
 
-<form method="POST" ">
+<form method="POST" onsubmit="publish()">
 <textarea id="post" title="Millest mõtled?" name="post" placeholder="Millest mõtled?"   style="height: 48px;"></textarea>
 <button  id="add" >Postita</button>
 </form>
