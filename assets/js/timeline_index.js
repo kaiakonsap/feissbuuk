c37c614e50c955aade67fa242e1fd3d10e389cc6
kaia <kaia.konsap@khk.ee>
2013-05-30 12:29:01 +0300

    Fix comments,  posts broken, double id used for the first 2 comments

--- assets/js/timeline_index.js
+++ assets/js/timeline_index.js
@@ -10,8 +10,7 @@ function publish() {
 		data   : "&post=" + document.getElementById('post').value,
 		success: function (data) {
 		//create a container for the new comment
-			$("<div>").addClass("well well-small").text($("#my_post").find("textarea").val()).appendTo('#posts').children()
-				.hide().fadeIn("slow");
+			$("<div>").addClass("well well-small").text($("#my_post").find("textarea").val()).appendTo('#posts').hide().fadeIn("slow");
 
 			//empty inputs
 			$("#my_post").find("textarea").val("");
@@ -28,13 +27,11 @@ function publish_comment(post_id) {
 		data   : "&comment=" + document.getElementById('comment').value,
 		success: function (data) {
 			//create a container for the new comment
-			$("<div>").addClass("well well-small").text($("#my_comment").find("textarea").val()).appendTo('#comments').children()
-				.hide().fadeIn("slow");
+			$("<div>").addClass("well well-small").text($('#'+post_id).find("textarea").val()).appendTo('#'+post_id+'com').hide().fadeIn("slow");
 
 			//empty inputs
-			$("#my_comment").find("textarea").val("");
+			$('#'+post_id).find("textarea").val("");
 		}
 	};
 	$.ajax(ajaxOpts);
 };
-
--- views/timeline_index_view.php
+++ views/timeline_index_view.php
@@ -7,32 +7,34 @@
 <textarea id="post" title="Millest mõtled?" name="post" placeholder="Millest mõtled?"   style="height: 48px;"></textarea>
 <button  onclick="publish()" id="add" >Postita</button>
 </div>
-<div id="posts"><ul style="width: 20%"></ul></div>
+<div id="posts"></div>
 
 	<div id="old_posts">
 
 			<?if(isset($posts[0]['text'])): foreach($posts as $post):?>
-					<div style="padding: 10px; text-align:left;background-color:powderblue; width: 33%" class="well">
+					<div style="padding: 10px; text-align:left;background-color:powderblue; width: 33%" class="well" id="<?=$post['post_id']?>">
 						<img style="float: left; margin-right: 20px;" src="<?=ASSETS_URL?>images/q.jpg" alt="profile pic">
 						<p> <?=$post['text']?></p>
 						<button   >meeldib!</button>
-						<button  >kommenteeri!</button>
+						<button  onclick="comment(<?=$post['post_id']?>)">kommenteeri!</button>
 						<img style="float: right; margin-right: 10px;" src="<?=ASSETS_URL?>images/like.png" alt="like pic">
 
-					</div>
+
 					<? if(isset($post[0]['comment'])):foreach($post[0]['comment'] as	$comment):?>
-							<div style="padding: 10px;margin-top: 0px;background-color: lightcoral; text-align:left; width: 33%
-							"class="well">
+							<div style="padding: 10px;margin-top: 0px;background-color: lavenderblush; text-align:left;"class="well">
 									<p> <?=$comment['text']?></p>
 							</div>
 					<?endforeach;endif?>
 
-						<div id="comments"><ul style="width: 20%"></ul></div>
-							<div id="my_comment">
+						<div id="comments"></div>
+							<div id="<?=$post['post_id']?>">
+								<span></span>
+								<div id="<?=$post['post_id']?>com"></div>
 								<textarea id="comment" title="Kommenteeri" name="comment" placeholder="Kommenteeri"   style="height:
 					48px;"></textarea>
 							<button  onclick="publish_comment(<?=$post['post_id']?>)" id="add" >Postita</button>
 						</div>
+					</div>
 			<?endforeach;endif?>
 
 	</div>
